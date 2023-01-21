19b6b73abc1cf488ab9a1d83f3fb3380a21a3f68
sphhax <zskaistis2@gmail.com>
2023-01-20 22:24:42 -0600

    whoa

---
+++ modules/emotes.js
@@ -0,0 +1,12 @@
+class Emote {
+    constructor(name, source, image) {
+        this.name = name;
+        this.source = source;
+        this.image = image;
+    } 
+}
+
+function clearEmotes() {
+    let template = {};
+    
+}
